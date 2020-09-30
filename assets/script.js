f3a6c2ca0b5a9dd2e53dc21b9e1008ac12813726
songhee <thdgml5537@gmail.com>
2020-09-30 01:58:04 -0400

    added script.js with jquery test

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    console.log('this is loading');
+    // DOM VARIABLES
+
+    // JS VARIABLES
+    
+    // FUNCTION DEFINITIONS
+
+    // FUNCTION CALLS
+
+    // EVENT LISTENERS
+})
